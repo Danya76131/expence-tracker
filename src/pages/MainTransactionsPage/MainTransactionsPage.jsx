83ed80b53d7216490c4395610a5256eb5c4e8300
Stanislav <17samsonov@gmail.com>
2025-07-04 13:48:45 +0300

    create mainPage title/Total amount

--- src/pages/MainTransactionsPage/MainTransactionsPage.jsx
+++ src/pages/MainTransactionsPage/MainTransactionsPage.jsx
@@ -1,7 +1,21 @@
+import Container from "../../components/Container/Container";
+import Section from "../../components/Section/Section";
+import TransactionsTotalAmount from "../../components/TransactionsTotalAmount/TransactionsTotalAmount";
 import styles from "./MainTransactionsPage.module.css";
 
 const MainTransactionsPage = () => {
-  return <div className={styles.wrapper}>MainTransactionsPage</div>;
+  return (
+    <Section>
+      <Container>
+        <h2 className={styles.mainTitle}>Expense Log</h2>
+        <p className={styles.mainText}>
+          Capture and organize every penny spent with ease! A clear view of your
+          financial habits at your fingertips.
+        </p>
+        <TransactionsTotalAmount />
+      </Container>
+    </Section>
+  );
 };
 
 export default MainTransactionsPage;
