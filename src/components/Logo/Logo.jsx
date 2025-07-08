ff758c849c6fa25f84ed93c81db86c290ac0be2a
Danya76131 <danylo.galetskiy@gmail.com>
2025-07-08 15:21:35 +0200

    add random decorationTab

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,19 @@
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { Link } from "react-router-dom";
 import Icon from "../UI/Icon/Icon";
 import css from "./Logo.module.css";
 
-const Logo = ({ isAuthorized }) => {
-  const redirectTo = isAuthorized ? "/main-transactions" : "/welcome";
+const Logo = () => {
+  const isAuthorized = useSelector(selectIsLoggedIn);
+  const redirectTo = isAuthorized ? "/transactions/expenses" : "/";
+
   return (
     <div className={css.logoWrapper}>
-      <a href={redirectTo}>
+      <Link to={redirectTo}>
         <Icon className={css.logoIcon} />
         <span className={css.logoText}>ExpenseTracker</span>
-      </a>
+      </Link>
     </div>
   );
 };
