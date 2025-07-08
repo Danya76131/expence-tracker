db50343d3a774ed92cd7f1688beee0089eca4cde
Andrii Lesnicov <andriilesnicov@gmail.com>
2025-07-08 16:42:08 +0200

    add modal styles, closeBtn, Logout mod &other

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import css from "./LogoutModal.module.css";
+import Icon from "../UI/Icon/Icon";
 
-const LogOutModal = () => {
-  return <div>LogOutModal</div>;
+const LogoutModal = ({ onLogout, onCancel }) => {
+  return (
+    <div className={css.backdrop}>
+      <div className={css.modal}>
+        <button
+          type="button"
+          className={css.closeBtn}
+          onClick={onCancel}
+          aria-label="Close logout modal"
+        >
+          <Icon name="close" size={20} stroke="white" />
+        </button>
+
+        <p className={css.text}>Are you sure you want to log out?</p>
+
+        <div className={css.actions}>
+          <button type="button" className={css.logoutBtn} onClick={onLogout}>
+            Log out
+          </button>
+          <button type="button" className={css.cancelBtn} onClick={onCancel}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default LogOutModal;
+export default LogoutModal;
